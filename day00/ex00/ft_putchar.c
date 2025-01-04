99d27375a7f54942f789411fae6ecf1a4a792c6d
mkinnear@42.fr <31743388+mkinnear@users.noreply.github.com>
2025-01-04 14:29:06 +0200

    Delete day00 directory

--- day00/ex00/ft_putchar.c
+++
@@ -1,7 +0,0 @@
-#include "../includes/day00.h"
-
-// This function writes the character passed to it
-void    ft_putchar(char c)
-{
-    write(1, &c, 1); // This function uses file descriptor 1 (typically used for console output) and takes in a voud* as well as the number of elements to print
-}
